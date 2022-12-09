04e9a935cb2b07def44cf72cfff2658ff58281fc
maxvonlancaster <windchange7@gmail.com>
2022-12-09 14:17:53 +0200

    sln added

---
+++ .net/Problems/Problems/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Problems.Services;
+
+Console.WriteLine("Hello, World!");
+var service = new LanguageGenerator();
+service.Main();
---
+++ .net/Problems/Problems/Services/LanguageGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Services
+{
+    public class LanguageGenerator
+    {
+        public void Main() 
+        {
+            Console.WriteLine("LanguageGenerator");
+        }
+    }
+}
