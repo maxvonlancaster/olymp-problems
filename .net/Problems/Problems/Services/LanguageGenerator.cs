65ca0abb6eb32e0362dd107bcd6e241d3842f8db
maxvonlancaster <windchange7@gmail.com>
2022-12-15 17:19:59 +0200

    todo updated

--- .net/Problems/Problems/Services/LanguageGenerator.cs
+++ .net/Problems/Problems/Services/LanguageGenerator.cs
@@ -2,17 +2,32 @@
 {
     public class LanguageGenerator
     {
-        private string[] vowels = new string[]{"a", "e", "y", "u", "i", "o"};
-        private string[] consonants = new string[]{"q", "w", "r", "t", "p", "s", "d", "f", "g", "h",
+        private string[] _vowels = new string[]{"a", "e", "y", "u", "i", "o"};
+        private string[] _consonants = new string[]{"q", "w", "r", "t", "p", "s", "d", "f", "g", "h",
         "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
 
-        private string[] syllables = new string[]{ };
-
+        private string[] _syllables = new string[]{ };
+        private Dictionary<string, string> _inclinations = new Dictionary<string, string>()
+        {
+            {"I", ""},
+            {"You", ""},
+            {"He", ""},
+            {"She", ""},
+            {"It", ""},
+            {"We", ""},
+            {"They", ""}
+        };
 
 
         public void Main() 
         {
             Console.WriteLine("LanguageGenerator");
         }
+
+        public void GenerateSyllables(){ }
+
+        public void GenerateInclination(){ }
+
+        
     }
 }
