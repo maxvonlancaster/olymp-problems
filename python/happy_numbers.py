3fd1470ca4fc1d2c17338c8d1f9e7b53284c3fa2
maxvonlancaster <windchange7@gmail.com>
2022-12-30 01:35:26 +0200

    happy n_s

--- python/happy_numbers.py
+++ python/happy_numbers.py
@@ -1,8 +1,12 @@
-import math 
+import time
+
+# unhappy_list = [20, 89, 37, 16, 4, 42, 58, 145]
+unhap = []
+hap = []
 
 def get_squeres(i):
     res = 0
-    splits = str(i).split()
+    splits = list(str(i))
     for s in splits:
         res += int(s)* int(s)
     return res
@@ -10,13 +14,28 @@ def get_squeres(i):
 def calculate_squeres(i):
     list_unique = []
     a = i
+    first = get_squeres(i)
     while True:
         list_unique.append(a)
         a = get_squeres(a)
-        if a in list_unique:
+        # if a in list_unique:
+        if (a == 42) or (a in unhap):
+            a = 42
+            if(first not in unhap):
+                unhap.append(first)
+            break
+        if (a == 1) or (a in hap):
+            a = 1
+            if(first not in hap):
+                hap.append(first)
             break
     return a
 
-for i in range(1, 100):
+start = time.time()
+for i in range(1, 10000001):
     res = calculate_squeres(i)
     print(f'{i} : {res} \n')
+
+end = time.time()
+elapsed = end - start
+print(f'elapsed time: {elapsed} seconds')
