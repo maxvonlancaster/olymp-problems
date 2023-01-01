2c8ec1749305491a523de53ce0091ee2ee90fd09
maxvonlancaster <windchange7@gmail.com>
2023-01-02 01:38:39 +0200

    generator

--- python/generating_functions/generating_f.py
+++ python/generating_functions/generating_f.py
@@ -1,14 +1,24 @@
-def _function():
-    return 0
+def _function(t):
+    return t
 
-def _sequence_rule(i):
-    return i
+def _generating_function(sequence, t):
+    res = 0
+    for i in range(len(sequence)):
+        res += sequence[i] * pow(t, i)
+    return res
+
+def _sequence_rule(sequence):
+    if len(sequence) == 0:
+        return 1
+    if len(sequence) == 1:
+        return 1
+    return sequence[len(sequence) - 1] + sequence[len(sequence) - 2]
 
 def _get_sequence(n):
     sequence = []
     for i in range(n):
-        sequence.append(_sequence_rule(i))
+        sequence.append(_sequence_rule(sequence))
     return sequence
 
-
+print(_get_sequence(20))
 
