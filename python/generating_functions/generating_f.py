6deb78ab2ef3cb87d9eaca9247590bb88ee12649
maxvonlancaster <windchange7@gmail.com>
2023-01-11 10:12:11 +0200

    generating function

--- python/generating_functions/generating_f.py
+++ python/generating_functions/generating_f.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import math
 
 def _function(t):
-    return t
+    return 1/(1-t-t*t)
 
 def _generating_function(sequence, t):
     res = 0
@@ -24,5 +24,19 @@ def _get_sequence(n):
         sequence.append(_sequence_rule(sequence))
     return sequence
 
-print(_get_sequence(20))
+seq = _get_sequence(20)
+print(seq)
+
+# plot a generating function:
+a1 = np.arange(0.0, 0.6, 0.001)
+a2 = np.arange(0.0, 0.6, 0.001)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(a1, _generating_function(seq, a1), 'bo')
+
+plt.subplot(212)
+plt.plot(a2, _function(a2), '--r')
+
+plt.show()
 
